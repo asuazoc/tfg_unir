618dc1b0a80d36f66d345a2fc74a72070d074095
asuazoc <adriasuazo@gmail.com>
2026-01-30 00:14:41 +0100

    Update TPVConfig.php

--- tpv/TPVConfig.php
+++ tpv/TPVConfig.php
@@ -6,10 +6,10 @@
 //Código CVV2: 123
 //CIP: 123456
 $tpv_config=array(
-        'url_tpvv'     		=> 'https://sis-t.redsys.es:25443/sis/realizarPago',
+        'url_tpvv'     		=> 'https://sis-t.redsys.es:21234/sis/realizarPago',
         'urlMerchant'  		=> '/TPVResponse.php',
-        'secret'        	=> 'sq7HjrUOBfKmC576ILgskD5srU870gJ7',
-        'code'      		=> '327234688',
+        'secret'        	=> 'sq7HjrUOXXXXXXXXXD5srU870gJ7',
+        'code'      		=> '327XXXXX88',
         'name'              => 'TPV DE PRUEBAS',
         'terminal'          => '1',
         'currency'          => '978',
